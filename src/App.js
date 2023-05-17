fb6c49d9a0ff6ef010a6326c5c3dc69f5f9c1649
MrBoomDoom <kyleetoddturner@gmail.com>
2023-05-16 23:38:33 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useContext } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import './App.css'
+
+import Header from './components/Header'
+import Home from './components/Home'
+import Auth from './components/Auth'
+import Form from './components/Form'
+import Profile from './components/Profile'
+
+import AuthContext from './store/authContext'
+
+const App = () => {
+const authCtx = useContext(AuthContext)
+
+
+  return (
+    <div className='app'>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/auth' element={<Auth/>}/>
+        <Route path='/form' element={<Form/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='*' element={<Navigate to='/'/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
